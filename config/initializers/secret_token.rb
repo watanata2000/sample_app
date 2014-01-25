dfe174d302fa4a1cf3c90c2bea32e35c6f1131be
watanata2000 <watanata.since.sept.2007@gmail.com>
2014-01-25 10:54:42 +0900

    fix spell error

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -14,7 +14,7 @@ require 'securerandom'
 def secure_token
 	token_file = Rails.root.join('.secret')
 	if File.exist?(token_file)
-		File.raed(token_file).chomp
+		File.read(token_file).chomp
 	else
 		token = SecureRandom.hex(64)
 		File.write(token_file, token)
