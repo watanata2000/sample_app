954c07f127535d7347d039090c395e5841b3a05c
watanata2000 <watanata.since.sept.2007@gmail.com>
2014-01-27 21:49:59 +0900

    do exercise 5.6

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,35 +6,56 @@ describe "Static Pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  # page link
+  it "should have the right link on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+
+  end
+
+
   # Home Page
   describe "Home page" do
     before { visit root_path }
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+    it_should_behave_like "all static pages"
     it { should_not have_title(full_title('| Home')) }
   end
 
   # Help Page
   describe "Help page" do
     before { visit help_path }
-
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
   end
 
   # About Page
   describe "About page" do
     before { visit about_path }
-
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About us' }
   end
 
   # Contact Page
   describe "Contact page" do
     before { visit contact_path }
-
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
   end
 end
