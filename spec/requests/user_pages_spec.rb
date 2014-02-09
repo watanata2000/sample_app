885a437031f48190dcea54773b1f4f05346f589f
watanata2000 <watanata.since.sept.2007@gmail.com>
2014-02-09 17:05:53 +0900

    create user's show page with gvavatar

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,4 +9,12 @@ describe "User Pages" do
   	it { should have_content('Sign up') }
   	it { should have_title(full_title('Sign up')) }
   end
+
+  describe "profile page" do
+  	# ユーザ変数を作成するためのコードに置き換える。
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+ end
 end
